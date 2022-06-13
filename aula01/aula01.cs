88681eedceb94c31312e9578fb2c03b72e942663
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2022-06-13 11:05:15 -0300

    youtube

---
+++ aula01/aula01.cs
@@ -0,0 +1,8 @@
+using System;// system é pra usar uma biblioteca system(entrada e saidas simples console, teclado, variaveis, etc)
+// criar nosso programa
+
+class Principal{
+    static void Main(string[] args){// classe principal ou programa principal
+       Console.WriteLine("estou aprendendo csharp...");// escrever no console
+    }
+}
