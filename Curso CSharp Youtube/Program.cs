3e1276b1dd12eab501ce82b5c3253f6f04cc86a7
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2022-06-14 11:07:30 -0300

    public class

--- Curso CSharp Youtube/Program.cs
+++ Curso CSharp Youtube/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace MeuPrograma
+{
+    class EstudoC{
+        static void Main(string[] args)
+    {
+        char c;
+        string str;
+        Console.WriteLine("digite seu nome");
+        str = Console.ReadLine();
+        Console.WriteLine("digite uma letra");
+        c = (char)Console.Read();
+        Console.WriteLine();
+        Console.WriteLine("seu nome é : {0}", str);
+        Console.WriteLine("a letra é : {0}", c.ToString());
+        Console.ReadLine();    
+
+    }
+        }
+}
